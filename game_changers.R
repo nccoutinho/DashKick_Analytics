09e01b793a22cb3cfc22632858afde91942b8d4b
Pranav Kumar Mahesh <pranava.km@gmail.com>
2024-01-25 20:30:58 -0800

    display table

--- game_changers.R
+++ game_changers.R
@@ -1,13 +1,55 @@
+library(httr)
+library(tidyjson)
+library(dplyr)
+library(jsonlite)
+
+url <- "https://api-football-v1.p.rapidapi.com/v3/players"
+
+all_players <- list()
+
+for(page in 1:47)
+{
+  queryString <- list(
+    league = "39",
+    season = "2023",
+    page = page
+  )
+  
+  response <- VERB("GET", url, query = queryString, add_headers('X-RapidAPI-Key' = '1272d4dfaamshea38349fbd93df4p178e05jsn2804b1438ab3', 'X-RapidAPI-Host' = 'api-football-v1.p.rapidapi.com'), content_type("application/octet-stream"))
+  
+  json_string <- content(response, "text")
+  
+  json_data <- fromJSON(json_string)
+  
+  player_stats <- json_data$response
+  
+  df <- data.frame(
+    PlayerName = paste(player_stats$player$firstname, player_stats$player$lastname),
+    PlayerAge = player_stats$player$age,
+    Appearances = sapply(player_stats$statistics, function(stat) sum(stat$games$appearences, na.rm = TRUE)),
+    Goals = sapply(player_stats$statistics, function(stat) sum(stat$goals$total, na.rm = TRUE)),
+    Assists = sapply(player_stats$statistics, function(stat) sum(stat$goals$assists, na.rm = TRUE)),
+    YellowCards = sapply(player_stats$statistics, function(stat) sum(stat$cards$yellow, na.rm = TRUE)),
+    RedCards = sapply(player_stats$statistics, function(stat) sum(stat$cards$red, na.rm = TRUE))
+  )
+  
+  all_players <- rbind(rbind(all_players, df)
+  )
+}
+
+
+all_players
+
+
+
+all_players$GoalsAssists = all_players$Goals + all_players$Assists
+
+
+df_ordered <- all_players[order(all_players$GoalsAssists, decreasing = TRUE), ]
+
 library(DT)
 library(shiny)
 
-# Sample data for 10 players
-top_scorers <- data.frame(
-  Rank = 1:10,
-  Player = c("Harry Kane", "Mohamed Salah", "Bruno Fernandes", "Patrick Bamford", "Son Heung-Min", "Dominic Calvert-Lewin", "Ollie Watkins", "Jamie Vardy", "Callum Wilson", "Wilfried Zaha"),
-  Club = c("Tottenham Hotspur", "Liverpool", "Manchester United", "Leeds United", "Tottenham Hotspur", "Everton", "Aston Villa", "Leicester City", "Newcastle United", "Crystal Palace"),
-  Goals = c(19, 17, 16, 15, 14, 14, 13, 12, 10, 9)
-)
 
 # Define the UI for the Shiny app
 ui <- fluidPage(
@@ -20,7 +62,7 @@ server <- function(input, output) {
   # Render the interactive table with improved styling
   output$goal_scorers_table <- renderDT({
     datatable(
-      top_scorers,
+      df_ordered,
       options = list(
         paging = TRUE,
         lengthMenu = c(5, 10, 15),
