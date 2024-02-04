d4dec606aabf9b2ece09bfc1ba255574b3d46f66
Pranav Kumar Mahesh <pranava.km@gmail.com>
2024-02-04 11:44:22 -0800

    minor change

--- game_changers.R
+++ game_changers.R
@@ -19,7 +19,6 @@ game_changers <- function() {
 
     response <- VERB("GET", url, query = queryString, add_headers('X-RapidAPI-Key' = '1272d4dfaamshea38349fbd93df4p178e05jsn2804b1438ab3', 'X-RapidAPI-Host' = 'api-football-v1.p.rapidapi.com'), content_type("application/octet-stream"))
     json_string <- content(response, "text", encoding='UTF-8')
-    print(json_string)
     json_data <- fromJSON(json_string)
     player_stats <- json_data$response
     
