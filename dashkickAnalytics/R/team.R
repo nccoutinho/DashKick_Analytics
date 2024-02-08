b24ee3896be51e4a4ccdb30783ba25bc88c82e4c
nccoutinho <natscc@student.ubc.ca>
2024-02-08 10:59:24 -0800

    added errorhandling for predict_win()

--- dashkickAnalytics/R/team.R
+++ dashkickAnalytics/R/team.R
@@ -90,22 +90,27 @@ game_changers <- function() {
 #'
 #' @export
 predict_win <- function() {
-  url <- "https://api-football-v1.p.rapidapi.com/v3/fixtures"
-
-  queryString <- list(
-    league = "39",
-    season = "2023"
-  )
-
-  response <- VERB("GET", url, query = queryString, add_headers('X-RapidAPI-Key' = '1272d4dfaamshea38349fbd93df4p178e05jsn2804b1438ab3', 'X-RapidAPI-Host' = 'api-football-v1.p.rapidapi.com'), content_type("application/octet-stream"))
-
-  json_string <- content(response, "text", encoding='UTF-8')
-
-  json_data <- fromJSON(json_string)
-
-  json_data <- fromJSON(json_string)
-
-  match_stats <- json_data$response
+  tryCatch({
+     url <- "https://api-football-v1.p.rapidapi.com/v3/fixtures"
+  
+    queryString <- list(
+      league = "39",
+      season = "2023"
+    )
+  
+    response <- VERB("GET", url, query = queryString, add_headers('X-RapidAPI-Key' = '1272d4dfaamshea38349fbd93df4p178e05jsn2804b1438ab3', 'X-RapidAPI-Host' = 'api-football-v1.p.rapidapi.com'), content_type("application/octet-stream"))
+  
+    json_string <- content(response, "text", encoding='UTF-8')
+  
+    json_data <- fromJSON(json_string)
+  
+    json_data <- fromJSON(json_string)
+  
+    match_stats <- json_data$response
+  }, error = function(e){
+    message('An error occured: ', e$message)
+    return (NULL)
+  })
 
 
 
